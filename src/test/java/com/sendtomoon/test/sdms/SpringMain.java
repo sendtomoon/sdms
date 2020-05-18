e1794ec39bc67146e97394ee8ae981723d7077ee
galaxysha512 <galaxysha512@gmail.com>
2020-05-18 20:17:57 +0800

    commit

--- src/test/java/com/sendtomoon/test/sdms/SpringMain.java
+++ src/test/java/com/sendtomoon/test/sdms/SpringMain.java
@@ -10,20 +10,20 @@ import com.sendtomoon.sdms.annotation.MicroService;
 public class SpringMain {
 
 	public static void main(String[] args) {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
+		ApplicationContext ac = new ClassPathXmlApplicationContext("spring-base.xml");
 		String[] names = ac.getBeanDefinitionNames();
 		for (String name : names) {
 			Class<?> beanClass = ac.getType(name);
 			Method[] ms = beanClass.getMethods();
-			for(Method m : ms) {
+			for (Method m : ms) {
 				MicroService an = m.getAnnotation(MicroService.class);
-				if(an==null) {
+				if (an == null) {
 					continue;
 				}
 				System.err.println(an.name());
 			}
 		}
-		
+
 	}
 
 }
