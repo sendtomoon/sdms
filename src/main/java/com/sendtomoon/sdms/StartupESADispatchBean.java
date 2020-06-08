9a7ba569e993b7d106054a7d6bb715c22a342900
galaxysha512 <galaxysha512@gmail.com>
2020-06-08 20:51:33 +0800

    commit

--- src/main/java/com/sendtomoon/sdms/StartupESADispatchBean.java
+++ src/main/java/com/sendtomoon/sdms/StartupESADispatchBean.java
@@ -1,6 +1,7 @@
 package com.sendtomoon.sdms;
 
 import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,39 +14,46 @@ import org.springframework.context.ApplicationContextAware;
 import com.sendtomoon.sdms.annotation.MicroService;
 
 
-public class StartupESADispatchBean implements InitializingBean{
+public class StartupESADispatchBean implements InitializingBean,ApplicationContextAware{
 
 	private ConcurrentHashMap<String, Properties> microServices = new ConcurrentHashMap<String, Properties>();
 	
+	private ApplicationContext applicationContext;
+	
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-			String[] names = applicationContext.getBeanDefinitionNames();
-			for (String name : names) {
-				Class<?> beanClass = applicationContext.getType(name);
-				Method[] ms = beanClass.getMethods();
-				for (Method m : ms) {
-					MicroService an = m.getAnnotation(MicroService.class);
-					if (an == null) {
-						continue;
-					}
-					Properties p = new Properties();
-					p.setProperty("name", an.name());
-					p.setProperty("value", an.value());
-					p.setProperty("group", an.group());
-					p.setProperty("weight", String.valueOf(an.weight()));
-					microServices.put(an.name(), p);
-				}
-			}
+		if(applicationContext==null) {
+			this.applicationContext=applicationContext;
+		}
+			
 	}
 	
 	private void toDubbo() {
-		for(Set<String> key : microServices.entrySet()) {
+		for(ConcurrentHashMap.Entry<String, Properties> entry : microServices.entrySet()) {
 			
 		}
 	}
 
 	public void afterPropertiesSet() throws Exception {
-		// TODO Auto-generated method stub
-		
+		if(applicationContext==null) {
+			this.setApplicationContext(applicationContext);
+		}
+		String[] names = applicationContext.getBeanDefinitionNames();
+		for (String name : names) {
+			Class<?> beanClass = applicationContext.getType(name);
+			Method[] ms = beanClass.getMethods();
+			for (Method m : ms) {
+				MicroService an = m.getAnnotation(MicroService.class);
+				if (an == null) {
+					continue;
+				}
+				Properties p = new Properties();
+				p.setProperty("name", an.name());
+				p.setProperty("value", an.value());
+				p.setProperty("group", an.group());
+				p.setProperty("weight", String.valueOf(an.weight()));
+				microServices.put(an.name(), p);
+			}
+		}
 	}
 
 }
